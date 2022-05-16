1beb57565aaf418014a5b51e948aae723498b85e
EC2 Default User <ec2-user@ip-172-31-80-182.ec2.internal>
2022-05-16 05:36:40 +0000

    [Finish]3-12

---
+++ app/helpers/hoomes_helper.rb
@@ -0,0 +1,2 @@
+module HoomesHelper
+end
---
+++ test/controllers/hoomes_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class HoomesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
